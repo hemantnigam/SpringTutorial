9df210bcc9d5830e39c5ef1746921d2eeb012fef
hemantnigam <h.nigam654@gmail.com>
2017-08-04 05:58:46 -0400

    Autowiring

---
+++ Autowiring/src/Address.java
@@ -0,0 +1,20 @@
+
+public class Address {
+	private String state;
+	private String country;
+	public String getState() {
+		return state;
+	}
+	public void setState(String state) {
+		this.state = state;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String toString() {
+		return state+" "+country;
+	}
+}
